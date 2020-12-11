0cfed24e6aaca485b18b7338de48b49c7584b5cd
Aman Bhardwaj <inblack>
2020-12-11 21:06:52 +0530

    cookies

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,15 +1,15 @@
 import { createWithApollo } from "./withApollo";
 import { ApolloClient, InMemoryCache } from "@apollo/client";
-import { PaginatedPosts } from "../src/generated/graphql";
+import { PaginatedPosts } from "./generated/graphql";
 import { NextPageContext } from "next";
 
 const createClient = ( ctx: NextPageContext ) =>
     new ApolloClient( {
-        uri: `${ process.env.NEXT_PUBLIC_SERVER_URL }/graphql`,
+        uri: process.env.NEXT_PUBLIC_SERVER_URL as string,
         credentials: "include",
         headers: {
             cookie:
-                ( typeof window === "undefined" ? ctx?.req?.headers.cookie : undefined ) ||
+                ( typeof window === "undefined" ? ctx.req?.headers.cookie : undefined ) ||
                 "",
         },
         cache: new InMemoryCache( {
