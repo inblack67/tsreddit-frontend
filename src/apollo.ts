5cff89cf0b30735ee1e113e710717640c1b9bdd9
Aman Bhardwaj <inblack>
2020-12-11 21:25:49 +0530

    graphql

--- src/apollo.ts
+++ src/apollo.ts
@@ -5,11 +5,11 @@ import { NextPageContext } from "next";
 
 const createClient = ( ctx: NextPageContext ) =>
     new ApolloClient( {
-        uri: process.env.NEXT_PUBLIC_SERVER_URL as string,
+        uri: `${ process.env.NEXT_PUBLIC_SERVER_URL}/graphql`,
         credentials: "include",
         headers: {
             cookie:
-                ( typeof window === "undefined" ? ctx.req?.headers.cookie : undefined ) ||
+                ( typeof window === "undefined" ? ( ctx && ctx.req?.headers.cookie ) : undefined ) ||
                 "",
         },
         cache: new InMemoryCache( {
